5325d39dfaac790f4f5993df23c7402a95129584
Rayan Bonte <rayan@MacBook-Pro.local>
2026-04-09 23:16:20 +0200

    Feature: Train verrouillé sans programme + date appel

--- src/App.jsx
+++ src/App.jsx
@@ -548,6 +548,7 @@ export default function App() {
       {programme && !authError && (
         <>
           {page === "training" ? (
+            !cloudProgramme ? <TrainLocked client={client} /> :
               <TrainingPage
                 client={client}
                 programme={programme}
